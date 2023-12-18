45f85fab057ff1fd10ccf2203dac3a839dbe3591
gaborloi <gaborloi@gmail.com>
2023-12-18 20:01:20 +0100

    fourth task

--- src/test/scala/TestTask4Suite.scala
+++ src/test/scala/TestTask4Suite.scala
@@ -4,21 +4,21 @@ import org.scalatest.funsuite.AnyFunSuite
 
 import scala.io.Source
 
-class TestTask3Suite extends AnyFunSuite {
+class TestTask4Suite extends AnyFunSuite {
 
-  test("task3_1 test file") {
-    println(Task3.parseFile1(Source.fromResource("input_test_3.txt")))
+  test("task4_1 test file") {
+    println(Task4.parseFile1(Source.fromResource("input_test_4.txt")))
   }
 
-  test("task3_1 all file") {
-    println(Task3.parseFile1(Source.fromResource("input_all_3.txt")))
+  test("task4_1 all file") {
+    println(Task4.parseFile1(Source.fromResource("input_all_4.txt")))
   }
 
-  test("task3_2 test file") {
-    println(Task3.parseFile2(Source.fromResource("input_test_3.txt")))
+  test("task4_2 test file") {
+    println(Task4.parseFile2(Source.fromResource("input_test_4.txt")))
   }
 
-  test("task3_2 all file") {
-    println(Task3.parseFile2(Source.fromResource("input_all_3.txt")))
+  test("task4_2 all file") {
+    println(Task4.parseFile2(Source.fromResource("input_all_4.txt")))
   }
 }
