10ff7d888daeab225e2364c801fb10754ae3c4f3
gaborloi <gaborloi@gmail.com>
2023-12-21 11:07:21 +0100

    task 6 second

--- src/main/scala/Task6.scala
+++ src/main/scala/Task6.scala
@@ -8,23 +8,34 @@ import scala.io.BufferedSource
 
 object Task6 {
   def EPS = 0.01
-  case class Race(time: Int, record: Int) {
-    def dist(chargeTime: Int) = chargeTime * math.min(time - chargeTime, 0)
+  case class Race(time: Long, record: Long) {
+    def dist(chargeTime: Long) = chargeTime * math.min(time - chargeTime, 0)
 
-    def recordCount(): Int = {
+    def recordCount(): Long = {
       val d = math.sqrt(time * time - 4 * record)
       val x1 = (time - d)/2.0
       val x2 = (time + d)/2.0
-      (x2 - EPS).toInt - (x1 + EPS).toInt
+      println(x1, x2)
+      (x2 - EPS).toLong - (x1 + EPS).toLong
     }
   }
 
-  def parseFile1(file: BufferedSource): Int = {
+  def parseFile1(file: BufferedSource): Long = {
     val lines = file.getLines().toList
-    val times = lines.head.substring(11).split(" +").toList
-    val records = lines(1).substring(11).split(" +").toList
-    val races: List[Race] = times.zip(records).map { r => Race(r._1.toInt, r._2.toInt)}
-    races.foldLeft(1) { (p ,r) => println(r.recordCount())
+    val times = lines.head.substring(11).trim.split(" +").toList
+    val records = lines(1).substring(11).trim.split(" +").toList
+    val races: List[Race] = times.zip(records).map { r => Race(r._1.toLong, r._2.toLong)}
+    races.foldLeft(1L) { (p ,r) => println(r.recordCount())
+      p * r.recordCount() }
+  }
+
+  def parseFile2(file: BufferedSource): Long = {
+    val lines = file.getLines().toList
+    val times = List(lines.head.substring(11).replace(" ", ""))
+    val records = List(lines(1).substring(11).replace(" ", ""))
+    val races: List[Race] = times.zip(records).map { r => Race(r._1.toLong, r._2.toLong)}
+
+    races.foldLeft(1L) { (p ,r) => println(r.recordCount())
       p * r.recordCount() }
   }
 }
--- src/test/scala/TestTask6Suite.scala
+++ src/test/scala/TestTask6Suite.scala
@@ -8,17 +8,17 @@ class TestTask6Suite extends AnyFunSuite {
 
   test("task6_1 test file") {
     println(Task6.parseFile1(Source.fromResource("input_test_6.txt")))
-  } // 3150
+  } // 288
 
-//  test("task5_1 all file") {
-//    println(Task5.parseFile1(Source.fromResource("input_all_5.txt")))
-//  } // 51 580 674
-//
-//  test("task5_2 test file") {
-//    println(Task5.parseFile2(Source.fromResource("input_test_5.txt")))
-//  } // 46
-//
-//  test("task5_2 all file") {
-//    println(Task5.parseFile2(Source.fromResource("input_all_5.txt")))
-//  } // 99 751 240
+  test("task6_1 all file") {
+    println(Task6.parseFile1(Source.fromResource("input_all_6.txt")))
+  } // 861300
+
+  test("task6_2 test file") {
+    println(Task6.parseFile2(Source.fromResource("input_test_6.txt")))
+  } // 71503
+
+  test("task6_2 all file") {
+    println(Task6.parseFile2(Source.fromResource("input_all_6.txt")))
+  } // 28 101 347
 }
