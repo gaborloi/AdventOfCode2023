9cb26b78cedc76db9acf609accace3f2d2cc4d22
gaborloi <gaborloi@gmail.com>
2023-12-21 08:53:19 +0100

    fifth task

--- src/main/scala/Task5.scala
+++ src/main/scala/Task5.scala
@@ -1,45 +1,144 @@
 package org.practice.advent
 
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 import scala.io.BufferedSource
-import scala.util.matching.Regex
 
-object Task4 {
-private val gamePattern: Regex = "Card\\s*\\d*:\\s*(.*) \\|\\s*(.*)".r
-  def parseLine(input: String): Int = {
-    gamePattern.findFirstMatchIn(input) match {
-      case Some(m) =>
-        val winnings = m.group(1)
-        val own = m.group(2)
-        val intersect = winnings.split(" +").toSet.intersect(own.split(" +").toSet)
-        println(input, intersect)
-      if (intersect.nonEmpty) math.pow(2, intersect.size - 1).toInt else 0
+object Task5 {
+
+  case class Range(start: Long, length: Long) {
+    def intersection(r2: Range): Option[Range] = {
+      val newStart = math.max(start, r2.start)
+      val newEnd = math.min(start + length, r2.start + r2.length)
+      val newLength = newEnd - newStart
+      newLength match {
+        case l if l > 0 => Some(Range(newStart, newLength))
+        case _ => None
+      }
+    }
+
+    def upperBound(): Long = start + length
+    def shift(diff: Long): Range = Range(start + diff, length)
+  }
+  case class DataMappingItem(destinationRangeStart: Long, sourceRangeStart: Long, length: Long) {
+    def sourceRange(): Range = Range(sourceRangeStart, length)
+//    def destinationRange(): Range = Range(destinationRangeStart, length)
+
+    def shiftValue(): Long = destinationRangeStart - sourceRangeStart
+
+    def reverse(): DataMappingItem = DataMappingItem(sourceRangeStart, destinationRangeStart, length)
+  }
+
+  class DataMapping(rawList: List[DataMappingItem]) {
+    val mappingList: List[DataMappingItem] = enrich(rawList)
+    def transform(rangeList: List[Range]): List[Range] = rangeList.flatMap {
+      r => mappingList.map( mi => mi.sourceRange().intersection(r).map(_.shift(mi.shiftValue())))
+    }.flatten
+
+    def reverse(): DataMapping = new DataMapping(
+      mappingList.map(_.reverse()).reverse
+    )
+
+    private def enrich(mappingList: List[DataMappingItem]): List[DataMappingItem] = {
+      val sortedMappingList = mappingList.sortBy {
+        _.sourceRangeStart
+      }
+      val enrichedList = sortedMappingList.foldLeft(List[DataMappingItem]()) { (dmList, dm) =>
+        val boundary = if (dmList.isEmpty) 0L else dmList.last.sourceRange().upperBound()
+        if (dm.sourceRangeStart != boundary) {
+          dmList ++ List(DataMappingItem(boundary, boundary, dm.sourceRangeStart - boundary), dm)
+        } else dmList ++ List(dm)
+      }
+      val finalBound = enrichedList.last.sourceRange().upperBound()
+      enrichedList :+ DataMappingItem(finalBound, finalBound, Long.MaxValue / 10)//-finalBound-1) //TODO
     }
   }
 
-  def parseLine2(idx: Int, maxIdx: Int, input: String, wonCardArray: Array[Int]): Array[Int] = {
-    gamePattern.findFirstMatchIn(input) match  {
-      case Some(m) =>
-        val winnings = m.group(1)
-        val own = m.group(2)
-        val intersect = winnings.split(" +").toSet.intersect(own.split(" +").toSet)
-        wonCardArray(idx) += 1
-        for (i <- idx + 1  to math.min(idx + intersect.size, maxIdx)) {
-          wonCardArray(i) += wonCardArray(idx)
+  case class DataMappingChain(mappingChain: List[DataMapping]) {
+    def reverse(): List[DataMapping] = mappingChain.map(_.reverse()).reverse
+
+    def transform(inputRanges: List[Range]): List[Range] = mappingChain.foldLeft(inputRanges) {
+      (nextRange, dataMapping) => println(nextRange)
+        dataMapping.transform(nextRange)
+    }
+  }
+  def parseFile1(file: BufferedSource): Long = {
+    var seed = List[Range]()
+    val mappings = ListBuffer[DataMapping]()
+    var currentList = ListBuffer[DataMappingItem]()
+    file.getLines() foreach {
+      case line@seeds if line.startsWith("seeds") => seed = parseSeeds(seeds)
+      case "" => if (currentList.nonEmpty) mappings.addOne(new DataMapping(currentList.toList))
+      case line if line(0).isLetter => currentList = ListBuffer[DataMappingItem]()
+      case line@digits if line(0).isDigit => currentList.addOne(parseMapping(digits))
+    }
+
+    val locations = findLocation(DataMappingChain(mappings.toList), seed)
+    println(locations)
+    locations.map(_.start).min
+  }
+
+  def findLocation(chain: DataMappingChain, seeds: List[Range]): List[Range] = chain.transform(seeds)
+
+  def parseMapping(input: String): DataMappingItem = {
+    val values = input.split(" ")
+    DataMappingItem(values(0).toLong, values(1).toLong, values(2).toLong)
+  }
+  //TODO: order seeds?
+  def parseSeeds(input: String): List[Range] = input.substring(7).split(" ").map(s => Range(s.toLong, 1L)).toList
+
+  def parseFile2(file: BufferedSource): Long = {
+    var seed = List[Range]()
+    val mappings = ListBuffer[DataMapping]()
+    var currentList = ListBuffer[DataMappingItem]()
+    file.getLines() foreach {
+      case line@seeds if line.startsWith("seeds") => seed = parseSeedsV2(seeds)
+      case "" => if (currentList.nonEmpty) mappings.addOne(new DataMapping(currentList.toList))
+      case line if line(0).isLetter => currentList = ListBuffer[DataMappingItem]()
+      case line@digits if line(0).isDigit => currentList.addOne(parseMapping(digits))
+    }
+
+    backwardMapping(DataMappingChain(mappings.toList), seed)
+  }
+
+  def parseSeedsV2(input: String): List[Range] = {
+    val seedInput = input.substring(7).split(" ").map(s => s.toLong).toList
+    val starts = seedInput.zipWithIndex.filter{ s => (s._2 % 2) == 0 }.map(_._1)
+    val lengths = seedInput.zipWithIndex.filter{ s => (s._2 % 2) == 1 }.map(_._1)
+
+    starts.zip(lengths).map { t => Range(t._1, t._2) }
+  }
+  def backwardMapping(chain: DataMappingChain, seeds: List[Range]): Long = {
+    val preppedChain = chain.reverse()
+    preppedChain.head.mappingList foreach  { dm =>
+      val seedRange = findRanges(1, List(dm.sourceRange()), preppedChain)
+      val availableSeeds = checkSeeds(seedRange, seeds)
+      availableSeeds match {
+        case Some(r) => return findLocation(chain, List(r)).head.start
+      }
+    }
+    -1
+  }
+
+  @tailrec
+  def findRanges(levelIdx: Int, destinationRange: List[Range], allDataMappings: List[DataMapping]): List[Range] = {
+    val sourceDataMappings = destinationRange flatMap { destRange =>
+      allDataMappings(levelIdx).transform(List(destRange))
+    }
+
+    if (levelIdx + 1 < allDataMappings.length) {
+      findRanges(levelIdx + 1, sourceDataMappings, allDataMappings)
+    } else sourceDataMappings
+  }
+
+  def checkSeeds(seedsToTest: List[Range], actualSeeds: List[Range]): Option[Range] = {
+    seedsToTest foreach { t =>
+      actualSeeds foreach { a =>
+        t.intersection(a) match {
+          case Some(r) => return Some(r)
         }
-        wonCardArray
+      }
     }
-  }
-
-  def parseFile1(file: BufferedSource): Int = file.getLines().foldLeft(0) {
-    (n,s) => parseLine(s) + n
-  }
-
-  def parseFile2(file: BufferedSource): Int = {
-    val lines = file.getLines().toList.zipWithIndex
-    val n = lines.size
-    val wonCardList = lines.foldLeft(new Array[Int](n)) {
-      (ll,idS) => parseLine2(idS._2, n - 1, idS._1, ll)
-    }
-    wonCardList.sum
+    None
   }
 }
--- src/test/scala/TestTask5Suite.scala
+++ src/test/scala/TestTask5Suite.scala
@@ -4,21 +4,21 @@ import org.scalatest.funsuite.AnyFunSuite
 
 import scala.io.Source
 
-class TestTask4Suite extends AnyFunSuite {
+class TestTask5Suite extends AnyFunSuite {
 
-  test("task4_1 test file") {
-    println(Task4.parseFile1(Source.fromResource("input_test_4.txt")))
+  test("task5_1 test file") {
+    println(Task5.parseFile1(Source.fromResource("input_test_5.txt")))
   }
 
-  test("task4_1 all file") {
-    println(Task4.parseFile1(Source.fromResource("input_all_4.txt")))
-  }
+  test("task5_1 all file") {
+    println(Task5.parseFile1(Source.fromResource("input_all_5.txt")))
+  } // 51 580 674
 
-  test("task4_2 test file") {
-    println(Task4.parseFile2(Source.fromResource("input_test_4.txt")))
-  }
-
-  test("task4_2 all file") {
-    println(Task4.parseFile2(Source.fromResource("input_all_4.txt")))
-  }
+//  test("task5_2 test file") {
+//    println(Task5.parseFile2(Source.fromResource("input_test_5.txt")))
+//  }
+//
+//  test("task5_2 all file") {
+//    println(Task5.parseFile2(Source.fromResource("input_all_5.txt")))
+//  }
 }
