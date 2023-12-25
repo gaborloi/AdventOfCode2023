13319c0047e3008fd200874d4a47f9bc62a03ecc
gaborloi <gaborloi@gmail.com>
2023-12-25 23:03:31 +0100

    task 11 second

--- src/main/scala/Task11.scala
+++ src/main/scala/Task11.scala
@@ -1,52 +1,52 @@
 package org.practice.advent
 
-import scala.annotation.tailrec
 import scala.io.BufferedSource
 
 object Task11 {
-  case class Galaxy(r: Int, c: Int, expansion: Int) {
-    def expandRow(idx: Int): Galaxy = if(r > idx) Galaxy(r + 1, c) else this
-    def expandCol(idx: Int): Galaxy = if(c > idx) Galaxy(r, c + 1) else this
+  case class Galaxy(r: Long, c: Long) {
+    def expandRow(idx: Long, expansion: Long): Galaxy = if(r > idx) Galaxy(r + 1L * expansion, c) else this
+    def expandCol(idx: Long, expansion: Long): Galaxy = if(c > idx) Galaxy(r, c + 1L * expansion) else this
 
-
-    def dist(g2: Galaxy): Int = math.abs(g2.r - r) + math.abs(g2.c - c)
+    def dist(g2: Galaxy): Long = math.abs(g2.r - r) + math.abs(g2.c - c)
   }
 
 
-  def expandUniverse(universe: Array[Array[Char]], galaxies: Set[Galaxy]): Set[Galaxy] = {
+  def expandUniverse(universe: Array[Array[Char]], galaxies: Set[Galaxy], expansion: Long): Set[Galaxy] = {
     val emptyRows = universe.zipWithIndex.filter { case (arr,_) => !arr.contains('#') }.map { _._2 }
     val emptyCols = universe.transpose.zipWithIndex.filter { case (arr,_) => !arr.contains('#') }.map { _._2 }
 
     val rowExpanded =  emptyRows.zipWithIndex.foldLeft(galaxies) { case (gs, (r, i)) =>
-      gs.map { g => g.expandRow(r + i) }
+      gs.map { g => g.expandRow(r + i * expansion, expansion) }
     }
-    emptyCols.zipWithIndex.foldLeft(rowExpanded) { case (gs, (c, i)) => gs.map {g => g.expandCol(c + i) } }
+    emptyCols.zipWithIndex.foldLeft(rowExpanded) { case (gs, (c, i)) =>
+      gs.map {g => g.expandCol(c + i * expansion, expansion) }
+    }
   }
 
   def collectGalaxies(universe: Array[Array[Char]]): Set[Galaxy] = (
-    for { r <- universe.indices; c <- universe.head.indices if universe(r)(c) == '#' } yield  Galaxy(r,c)
+    for { r <- universe.indices; c <- universe.head.indices if universe(r)(c) == '#' } yield  Galaxy(r.toLong,c.toLong)
   ).toSet
-  def calcFile1(file: BufferedSource): Int = {
+  def calcFile1(file: BufferedSource, expansion: Long): Long = {
     val lines = file.getLines().toList
     val universe = (for {
       line <- lines
     } yield line.toCharArray).toArray
     val galaxies = collectGalaxies(universe)
-    val expanded = expandUniverse(universe, galaxies).toList
-    expanded.combinations(2).foldLeft(0) {
+    val expanded = expandUniverse(universe, galaxies, expansion).toList
+    expanded.combinations(2).foldLeft(0L) {
       case (sd, Seq(g1, g2)) =>
         sd + g1.dist(g2)
     }
   }
 
-  def calcFile2(file: BufferedSource): Int = {
+  def calcFile2(file: BufferedSource, expansion: Long): Long = {
     val lines = file.getLines().toList
     val universe = (for {
       line <- lines
     } yield line.toCharArray).toArray
     val galaxies = collectGalaxies(universe)
-    val expanded = expandUniverse(universe, galaxies).toList
-    expanded.combinations(2).foldLeft(0) {
+    val expanded = expandUniverse(universe, galaxies, expansion).toList
+    expanded.combinations(2).foldLeft(0L) {
       case (sd, Seq(g1, g2)) =>
         sd + g1.dist(g2)
     }
--- src/test/scala/TestTask11Suite.scala
+++ src/test/scala/TestTask11Suite.scala
@@ -6,31 +6,19 @@ import scala.io.Source
 
 class TestTask11Suite extends AnyFunSuite {
 
-  test("task11 test file") {
-    println(Task11.calcFile1(Source.fromResource("input_test_11.txt")))
+  test("task11_1 test file") {
+    println(Task11.calcFile1(Source.fromResource("input_test_11.txt"), 1))
   } // 374
 
-  test("task11 all file") {
-    println(Task11.calcFile1(Source.fromResource("input_all_11.txt")))
-  } //
+  test("task11_1 all file") {
+    println(Task11.calcFile1(Source.fromResource("input_all_11.txt"), 1))
+  } // 9550717
 
-  test("task10b_1 test file") {
-    println(Task10.calcFile2(Source.fromResource("input_test_10_1.txt")))
-  } // 1
+  test("task11_2 test file") {
+    println(Task11.calcFile2(Source.fromResource("input_test_11.txt"), 99))
+  } // 8410
 
-  test("task10b_2 test file") {
-    println(Task10.calcFile2(Source.fromResource("input_test_10_2.txt")))
-  } // 1
-
-  test("task10b_3 test file") {
-    println(Task10.calcFile2(Source.fromResource("input_test_10_3.txt")))
-  } // 4
-
-  test("task10b_4 test file") {
-    println(Task10.calcFile2(Source.fromResource("input_test_10_4.txt")))
-  } // 4
-
-  test("task10_2 all file") {
-    println(Task10.calcFile2(Source.fromResource("input_all_10.txt")))
+  test("task11_2 all file") {
+    println(Task11.calcFile2(Source.fromResource("input_all_11.txt"), 999999))
   } //
 }
