3c95423a219e3a2fe259bfa9e99e2820f122da10
gaborloi <gaborloi@gmail.com>
2024-01-15 22:34:50 +0100

    task 21 buggy

--- src/main/scala/Task21.scala
+++ src/main/scala/Task21.scala
@@ -18,20 +18,27 @@ object Task21 {
       Cord(-1, -1)
     }
 
-    def evalCord(c: Cord, newValue: Char): Set[Cord] = {
+    def shiftGarden(dir: Cord): GardenMap = {
+      val newGarden = garden.map(_.clone())
+      for ( r <- garden.indices; c <- garden.head.indices) {
+        val cc = Cord(r, c) - dir
+        val gRow = if (cc.r % rowMax < 0) rowMax + (cc.r % rowMax) else cc.r % rowMax
+        val gCol = if (cc.c % colMax < 0) colMax + (cc.c % colMax) else cc.c % colMax
+        newGarden(r)(c) = garden(gRow)(gCol)
+      }
+      newGarden
+    }
+
+    def evalCord(c: Cord): Set[Cord] = {
       Cord.STEPS.foldLeft(Set[Cord]()) { (s, stp) =>
         val cord = c + stp
         if (!cord.valid(rowMax, colMax)) s
         else {
           val value = garden(cord.r)(cord.c)
           value match {
-            case c if newValue == c || c == '#' => s
-            case '.' =>
-              garden(cord.r)(cord.c) = newValue
-              s + cord
-            case 'S' =>
-              garden(cord.r)(cord.c) = newValue
-              s
+            case '#' => s
+            case '.' | 'S' => s + cord
+            case _ => s
           }
         }
       }
@@ -50,12 +57,37 @@ object Task21 {
     }
 
     @tailrec
-    final def step(maxStep:Int, currentStep: Int, lastNewPos: Set[Cord]): Int = {
-      val currentTurn = (currentStep % 2).toString.head
+    final def step(
+      maxStep:Int,
+      currentStep: Int,
+      prePos: Set[Cord],
+      preCount: Int,
+      preprePos: Set[Cord],
+      prepreCount: Int): (Int, Int) = {
+      val newPos = prePos.flatMap(pos => evalCord(pos)).diff(preprePos)
+      val newCount = prepreCount + newPos.size
+      if ((maxStep == currentStep + 1) || newPos.isEmpty) {
+        return if((maxStep - 1) % 2 == currentStep % 2) {
+          (newCount, preCount)
+        } else (preCount, newCount)
+      }
+      step(maxStep, currentStep + 1,  newPos, newCount, prePos, preCount)
+    }
 
-      val newPos = lastNewPos.flatMap(pos => evalCord(pos, currentTurn))
-      if (maxStep == currentStep + 1) return garden.map(arr => arr.count( p => p == currentTurn)).sum
-      step(maxStep,currentStep + 1,  newPos)
+    @tailrec
+    final def diffFromOptimal(
+      maxStep:Int,
+      currentStep: Int,
+      prePos: Set[Cord],
+      preprePos: Set[Cord],
+      newGardenMap: GardenMap
+    ): GardenMap = {
+      val newPos = prePos.flatMap(pos => evalCord(pos)).diff(preprePos)
+      newPos.foreach { c =>
+        newGardenMap(c.r)(c.c) = (currentStep + 1 - math.abs(c.r-startCord.r) - math.abs(c.c-startCord.c)).toString.head
+      }
+      if ((maxStep == currentStep + 1) || newPos.isEmpty) return newGardenMap
+      diffFromOptimal(maxStep, currentStep + 1,  newPos, prePos, newGardenMap)
     }
 
     @tailrec
@@ -69,7 +101,11 @@ object Task21 {
     ): Long = {
       val newPos = prePos.flatMap(pos => evalCordInf(pos)).diff(preprePos)
       val newCount = prepreCount + newPos.size.toLong
-      if (maxStep == currentStep + 1) return newCount
+      if ((maxStep == currentStep + 1) || newPos.isEmpty) {
+        return if ((maxStep - 1) % 2 == currentStep % 2) {
+          newCount
+        } else preCount
+      }
       stepInfinite(maxStep, currentStep + 1,  newPos, newCount, prePos, preCount)
     }
   }
@@ -89,19 +125,48 @@ object Task21 {
     val STEPS: List[Cord] = List(Cord(1, 0), Cord(0, 1), Cord(-1, 0), Cord(0, -1))
   }
 
+  def calcFullyCoveredGardens(garden: Garden, maxStep: Int): Long = {
+
+    val (evenCount, oddCount) = garden.step(10000, 0, Set(garden.startCord), 1, Set(), 0)
+
+    (1 until maxStep / garden.rowMax).foldLeft(0L) { (sum, idx) =>
+      val count = if(idx % 2 == 0) oddCount.toLong else evenCount.toLong
+      sum + idx.toLong * count
+    }
+  }
+
+  def calcQuarterPlane(garden: Garden, maxStep: Int): Long = {
+//    val fcg = calcFullyCoveredGardens(garden, maxStep)
+//    val fullRound = garden.step(garden.rowMax + maxStep % garden.rowMax, 0, Set(garden.startCord), 1, Set(), 0)._1 * (maxStep / garden.rowMax)
+//    val resid = if (maxStep % garden.rowMax > 0)
+//      garden.step(maxStep % garden.rowMax, 0, Set(garden.startCord), 1, Set(), 0)._1 * (maxStep / garden.rowMax + 1)
+//    else 1L //TODO
+    val (evenCount, oddCount) = garden.step(10000, 0, Set(garden.startCord), 1, Set(), 0)
+    var sum = 0L
+    for (r <- Range.inclusive(0, maxStep, 131); c <- Range.inclusive(0, maxStep, 131)) {
+      if (maxStep - r - c > 262) {
+        val count = if((r + c) % 2 == 0) evenCount.toLong else oddCount.toLong
+//        println(r,c, "full", count)
+        sum += count
+      } else if (maxStep - r - c > 0) {
+        val resid = garden.step(maxStep - r - c, 0, Set(garden.startCord), 1, Set(), 0)._1
+        garden.diffFromOptimal(maxStep - r - c, 0, Set(garden.startCord),Set(),garden.garden.map(_.clone())) foreach {arr => println(arr.mkString)}
+        println()
+//        println(r,c, "partial", resid)
+        sum += resid
+      } else if (maxStep - r - c == 0) sum += 1L
+    }
+    sum
+//    println(fcg, resid, fullRound)
+//    fcg + resid + fullRound
+  }
+
   def calcFile1(file: BufferedSource, maxStep: Int): Int = {
     val lines = file.getLines().toList
     val garden: Garden = Garden((for {
       line <- lines
     } yield line.toArray).toArray)
-//    garden.garden foreach (arr => println(arr.mkString))
-    val hashtagcount = garden.garden.foldLeft(0) { (s,arr) => s + arr.count(_ == '#') }
-    println(hashtagcount)
-    println(garden.rowMax, garden.colMax, garden.startCord)
-    println((131*131-hashtagcount)/2)
-    garden.step(maxStep, 0, Set(garden.startCord))
-    garden.garden foreach (arr => println(arr.mkString))
-    1
+    garden.step(maxStep, 0, Set(garden.startCord), 1, Set(), 0)._1
   }
 
   def calcFile2(file: BufferedSource, maxStep: Int): Long = {
@@ -109,7 +174,24 @@ object Task21 {
     val garden: Garden = Garden((for {
       line <- lines
     } yield line.toArray).toArray)
-    //    garden.garden foreach (arr => println(arr.mkString))
+
+    val rdG = garden.shiftGarden(Cord(0,0) - garden.startCord)
+    val luG = garden.shiftGarden(Cord(garden.rowMax - 1, garden.colMax - 1) - garden.startCord)
+    val ldG = garden.shiftGarden(Cord(0, garden.colMax - 1) - garden.startCord)
+    val ruG = garden.shiftGarden(Cord(garden.rowMax - 1, 0) - garden.startCord)
+
+    List(rdG, luG, ldG, ruG).foldLeft(0L) { (sum, gm) =>
+      val g = Garden(gm)
+
+      sum + calcQuarterPlane(g,maxStep)
+    } - (maxStep / 2 + 1) * 4 + 1 * (maxStep - 1) % 2
+  }
+
+  def calcFile3(file: BufferedSource, maxStep: Int): Long = {
+    val lines = file.getLines().toList
+    val garden: Garden = Garden((for {
+      line <- lines
+    } yield line.toArray).toArray)
     garden.stepInfinite(maxStep, 0, Set(garden.startCord), 1, Set(), 0)
   }
 }
--- src/test/scala/TestTask21Suite.scala
+++ src/test/scala/TestTask21Suite.scala
@@ -11,7 +11,7 @@ class TestTask21Suite extends AnyFunSuite {
   } // 16
 
   test("task21_1 all file") {
-    println(Task21.calcFile1(Source.fromResource("input_all_21.txt"), 1000))
+    println(Task21.calcFile1(Source.fromResource("input_all_21.txt"), 64))
   } // 3699
 
   test("task21_2 test file") {
@@ -19,6 +19,19 @@ class TestTask21Suite extends AnyFunSuite {
   } // 16733044
 
   test("task21_2 all file") {
-    println(Task21.calcFile2(Source.fromResource("input_all_21.txt"), 1000))
-  } //
+    println(Task21.calcFile2(Source.fromResource("input_all_21.txt"), 174))//26501365))
+  } // 613391963786150 too high 64-> 3699, 131 -> 15199, 200 -> 35361, 500 -> 219193, 600 -> 316048, 700 -> 428419 875527, 877767
+    // 613391294982479  429933
+
+  test("task21_2 all file slow") {
+    println(Task21.calcFile3(Source.fromResource("input_all_21.txt"), 175))//26501365))
+  } //131 -> 15199, 200 -> 35361, 500 -> 219193, 700 -> 428424, 875534, 877785
+
+  test("try") {
+    (1 to 710) foreach { i =>
+      val faster = Task21.calcFile2(Source.fromResource("input_all_21.txt"), i)
+      val slower = Task21.calcFile3(Source.fromResource("input_all_21.txt"), i)
+      if (faster != slower) println(i, faster, slower)
+    }
+  }
 }
